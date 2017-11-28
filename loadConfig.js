359cf90c8266678a5b37d5d980167e6d4ef79dd7
Tucker Connelly <web@tuckerconnelly.com>
2017-11-27 16:35:16 -0800

    Add loadConfig

---
+++ loadConfig.js
@@ -0,0 +1,34 @@
+const fs = require('fs')
+
+const dotenv = require('dotenv')
+const _ = require('lodash')
+const jsome = require('jsome')
+
+const envExample = dotenv.parse(fs.readFileSync('.env.example'))
+const env = dotenv.parse(fs.readFileSync('.env'))
+
+const difference = _.difference(Object.keys(envExample), Object.keys(env))
+
+if (difference.length !== 0) {
+  throw new Error(`You're missing an env var in .env that's set in .env.example: ${JSON.stringify(difference)}`)
+}
+
+Object.keys(env).forEach(key => {
+  if (process.env.hasOwnProperty(key)) return
+  process.env[key] = env[key]
+})
+
+const cleanConfig = _(process.env)
+  .pickBy((_, key) => !/^npm_/.test(key)) // Removes npm_* vars
+  .toPairs()
+  .sortBy(0)
+  .fromPairs() // Orders by key alphabetically
+  .value()
+
+if (process.env.NODE_ENV === 'production') {
+  console.log(`Starting with config: ${JSON.stringify(cleanConfig)}`)
+}
+if (process.env.NODE_ENV === 'development') {
+  console.log('Starting with config:')
+  jsome(cleanConfig)
+}
